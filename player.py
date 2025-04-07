e2bdacb55c6f44c4fe9b3ce9fc12941851d683d2
gryph853 <gryphonseyrie@outlook.com>
2025-04-06 19:59:15 -0700

    player x and y added

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
 import pygame
 from circleshape import CircleShape
+from constants import PLAYER_RADIUS
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.position = pygame.Vector2(x, y)
 
